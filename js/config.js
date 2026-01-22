7b68052fe1822581ebb0c85eea220e00b90fcfaf
RonaldJobse <ronald.jobse@vng.nl>
2026-01-22 12:45:45 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -18,7 +18,7 @@ let respecConfig = {
   //previousPublishDate: "2022-06-24",
   //previousPublishVersion: "0.0.4",
   
-  title: "Informatiemodel MijnGesprkken",
+  title: "Informatiemodel MijnGesprekken",
   // subtitle: "Tevens een voorbeeld van een gegenereerd Respec document",
   // content: {"mermaid": "", "ch01": "informative", "ch02": ""},
   authors:
