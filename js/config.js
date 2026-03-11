3c9c9156b43cc140dec5f00b87bf0f801a43e264
RonaldJobse <ronald.jobse@vng.nl>
2026-03-11 11:46:18 +0100

    Update config.js
    v0.2.1 2026-03-11

--- js/config.js
+++ js/config.js
@@ -10,8 +10,8 @@ let respecConfig = {
   
   // Zie de globale property 'localizationStrings/nl' voor de lijst met toegestane specificatie-statussen
   specStatus: "IO",
-  publishDate: "2026-03-10",
-  publishVersion: "0.2.0",
+  publishDate: "2026-03-11",
+  publishVersion: "0.2.1",
 
   // Zie de globale property 'localizationStrings/nl' voor de lijst met toegestane maturities
   //previousMaturity: "IO",
